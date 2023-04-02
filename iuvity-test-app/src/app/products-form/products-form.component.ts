151e06f49eacf5e1e1f6d63def6e7d32d05b3531
Manuel Sanchez <manuelsanchez98715022@gmail.com>
2023-04-02 18:27:52 -0500

    Enviar data para guardar producto en mongo

--- iuvity-test-app/src/app/products-form/products-form.component.ts
+++ iuvity-test-app/src/app/products-form/products-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-products-form',
@@ -14,4 +15,29 @@ export class ProductsFormComponent {
     priceProduct: 0
   } 
 
+  save(){
+    console.log('product-->', this.product);
+    let data = JSON.stringify({
+      "name": this.product.nameProduct,
+      "size": this.product.sizeProduct,
+      "brand": this.product.brandProduct,
+      "price": this.product.priceProduct
+    });
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/products',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    axios.request(config)
+    .then((response: { data: any; }) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });    
+  }
 }
